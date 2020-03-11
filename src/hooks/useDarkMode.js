e5148602e748cd06f874c2b18641053d3af45219
Landon Turner <lturner@LTurner-MBP.lan1>
2020-03-11 14:18:55 -0400

    going to lunch, need to walk through the readme again, button is broken, def not set up right

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,17 +1,20 @@
 import {useEffect} from 'react';
 import {useLocalStorage} from './useLocalStorage';
 
-export const useDarkMode = (key) => {
-    const [darkMode, setDarkMode] = useLocalStorage(key);
+export const useDarkMode = (key, initialValue) => {
+    const [darkMode, setDarkMode] = useLocalStorage(key, initialValue);
     useEffect(()=>{
         const body = document.getElementsByTagName("body")
         if(darkMode === true){
             return body.classList.add("dark-mode");
+            setDarkMode()
             //prob wrong here lol pick up after class
         }else{
-            return body.classList.add("dark-mode");
+            return null;
         }
     }, [darkMode])
     
+
+    return [darkMode]
 }
 
