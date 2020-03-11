9b06633d34f2ceb3f88d57c4c630d202bbce5b0a
Landon Turner <lturner@LTurner-MBP.lan1>
2020-03-11 16:37:53 -0400

    dark mode works

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -2,19 +2,23 @@ import {useEffect} from 'react';
 import {useLocalStorage} from './useLocalStorage';
 
 export const useDarkMode = (key, initialValue) => {
-    const [darkMode, setDarkMode] = useLocalStorage(key, initialValue);
+    console.log('key', key)
+    const [value, setValue] = useLocalStorage(key, initialValue);
     useEffect(()=>{
-        const body = document.getElementsByTagName("body")
-        if(darkMode === true){
-            return body.classList.add("dark-mode");
-            setDarkMode()
+        console.log(value)
+        if(value === true){
+            const bodyHTML = document.getElementsByTagName("body")
+            const body = bodyHTML[0].classList.add("dark-mode")
+            return body;
             //prob wrong here lol pick up after class
         }else{
-            return null;
+            const bodyHTML = document.getElementsByTagName("body")
+            const body = bodyHTML[0].classList.remove("dark-mode")
+            return body;
         }
-    }, [darkMode])
+    }, [value])
     
-
-    return [darkMode]
+    console.log('new value', value)
+    return [value, setValue]
 }
 
